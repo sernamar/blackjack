00aa0ec60977cbd9855174082483cf8d14977c65
sernamar <sernamar@protonmail.com>
2021-05-23 12:48:13 +0200

    Add an exmaple of how to play the first hand

--- src/blackjack.lisp
+++ src/blackjack.lisp
@@ -62,3 +62,27 @@ For now, I will consider that the value of Aces is 1 (it could be 1 or 11, depen
     (dotimes (x 2)
       (dolist (player players)
         (hit player deck)))))
+
+;;; ------------------------------- ;;;
+;;; Example of how to play the game ;;;
+;;; ------------------------------- ;;;
+
+;; Create a 3 players, 1 deck black jack game
+(defparameter game (make-game 3))
+
+;; Deal the first hand
+(deal-first-hand game)
+
+;; Get the list of players and their slots values
+(defparameter players (players game))
+
+(mapcar #'name players)   ;=> ("player-1" "player-2" "player-3")
+(mapcar #'hand players)   ;=> ((10 10) (2 3) (10 1))
+(mapcar #'points players) ;=> (20 5 11)
+
+(mapcar (lambda (player)
+          (format t "~a: ~d points~%" (name player) (points player)))
+        players)
+;=> player-1: 20 points
+;=> player-2: 5 points
+;=> player-3: 11 points
