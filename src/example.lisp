090f66c5e8958126eaed5822f1592e16156237b8
sernamar <sernamar@protonmail.com>
2021-06-19 10:07:19 +0200

    Print the points of the dealer and of each player after the first hand

--- src/example.lisp
+++ src/example.lisp
@@ -6,26 +6,14 @@
 ;; Deal the first hand
 (deal-first-hand *game*)
 
-;; Get the list of players and their slots values
-(defparameter *players* (players *game*))
-
-(mapcar #'name *players*)   ;=> ("player-1" "player-2" "player-3")
-(mapcar #'hand *players*)   ;=> ((10 10) (2 3) (10 1))
-(mapcar #'points *players*) ;=> (20 5 11)
-
-;; Get the dealer and his slots values
-(defparameter *dealer* (dealer *game*))
-
-(name *dealer*)
-(hand *dealer*)
-(points *dealer*)
-
-;; Print the point of each player after the first hand
-(mapcar (lambda (player)
-          (format t "~a: ~d points~%" (name player) (points player)))
-        *players*)
-(format t "~a: ~d points~%" (name *dealer*) (points *dealer*))
+;; Print the points of the dealer and of each player after the first hand
+(let ((players (players *game*))
+      (dealer (dealer *game*)))
+  (format t "~a: ~d points~%" (name dealer) (points dealer))
+  (mapcar (lambda (player)
+            (format t "~a: ~d points~%" (name player) (points player)))
+          players))
+;=> dealer: 11 points
 ;=> player-1: 20 points
 ;=> player-2: 5 points
 ;=> player-3: 11 points
-;=> dealer: 11 points
