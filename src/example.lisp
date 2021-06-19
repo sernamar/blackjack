3e98db0d6403604db0dffa590e28201afea2088d
sernamar <sernamar@protonmail.com>
2021-06-19 10:00:31 +0200

    Use the conventional Common Lisp notation for global variables

--- src/example.lisp
+++ src/example.lisp
@@ -1,20 +1,20 @@
 (in-package #:blackjack)
 
 ;; Create a 3 players, 1 deck black jack game
-(defparameter game (make-game 3))
+(defparameter *game* (make-game 3))
 
 ;; Deal the first hand
-(deal-first-hand game)
+(deal-first-hand *game*)
 
 ;; Get the list of players and their slots values
-(defparameter players (players game))
+(defparameter *players* (players *game*))
 
-(mapcar #'name players)   ;=> ("player-1" "player-2" "player-3")
-(mapcar #'hand players)   ;=> ((10 10) (2 3) (10 1))
-(mapcar #'points players) ;=> (20 5 11)
+(mapcar #'name *players*)   ;=> ("player-1" "player-2" "player-3")
+(mapcar #'hand *players*)   ;=> ((10 10) (2 3) (10 1))
+(mapcar #'points *players*) ;=> (20 5 11)
 
 ;; Get the dealer and his slots values
-(defparameter *dealer* (dealer game))
+(defparameter *dealer* (dealer *game*))
 
 (name *dealer*)
 (hand *dealer*)
@@ -23,7 +23,7 @@
 ;; Print the point of each player after the first hand
 (mapcar (lambda (player)
           (format t "~a: ~d points~%" (name player) (points player)))
-        players)
+        *players*)
 (format t "~a: ~d points~%" (name *dealer*) (points *dealer*))
 ;=> player-1: 20 points
 ;=> player-2: 5 points
